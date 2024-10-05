32885980cdbfb87dba8a0a3e0d24a7ce06138a16
kate127t <79634757+kate127t@users.noreply.github.com>
2024-10-05 15:56:56 +0200

    Add files via upload
    Interface of the Problem class added

---
+++ Problem.py
@@ -0,0 +1,31 @@
+'Project 3: Advanced Numerical Algorithms in Python'
+
+import numpy as np
+
+
+class Problem:
+    def __init__(self, width, length, delta_x, delt_y):
+        """
+        Initializes class Problem with given values.
+        
+        Parameters:
+            width: float, width
+            length: float, length
+            delta_x: float, stepsize of the grid
+            delta_y: float, stepsize of the grid
+
+        """
+
+    def update_boundary(new_values: np.array, i:int):
+        '''
+        Updates the boundary condition with new_values for the boundary i.
+        
+        Parameters:
+            new_values: np.array - new boundary condition
+            i: int, index of the boundary condition we need to change
+        
+        Returns:
+        -
+        '''
+
+        
