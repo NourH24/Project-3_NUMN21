fb777f7af5e2a89745d44bda0d0263c993181b10
Soederman <146443432+Soederman@users.noreply.github.com>
2024-10-07 20:49:18 +0200

    Update Main.py
    Started of with the main

--- Main.py
+++ Main.py
@@ -1,2 +1,37 @@
 #Main class
 #MPI
+import numpy as np
+from Problem import Problem
+from Method import Method
+
+if _name_ == '_main_':
+    mesh_size = 1/20
+    omega = 0.8
+    itterations = 10
+
+    num_points_boundary = 1/mesh_size # Correct?
+
+    problem1 = Problem(1, 1, mesh_size, mesh_size)
+    problem2 = Problem(1, 2, mesh_size, mesh_size)
+    problem3 = Problem(1, 1, mesh_size, mesh_size)
+
+    # Check index of walls
+    # Check what to init boundary walls with
+    problem1.update_boundary(np.ones(num_points_boundary) * 40, 0) # Left
+    problem1.update_boundary(np.ones(num_points_boundary) * 15, 2) # Top
+    problem1.update_boundary(np.ones(num_points_boundary) * 15, 3) # Bottom
+    
+    problem2.update_boundary(np.ones(num_points_boundary) * 40, 2) # Top
+    problem2.update_boundary(np.ones(num_points_boundary) * 5, 3) # Bottom
+
+    problem3.update_boundary(np.ones(num_points_boundary) * 15, 2) # Top
+    problem3.update_boundary(np.ones(num_points_boundary) * 15, 3) # Bottom
+    problem3.update_boundary(np.ones(num_points_boundary) * 40, 4) # Right
+
+    for i in range(itterations):
+        u2 = Method.solve(problem2)
+        
+        # Update 2, 3...
+
+        u1 = Method.solve(problem1)
+        u3 = Method.solve(problem3)
